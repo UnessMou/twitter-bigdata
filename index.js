91a57bdc13050643c14946acc23ab71176a8a862
Uness <youness.mou@gmail.com>
2017-04-20 17:40:57 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,30 @@
+//include Express
+const app = require('express')();
+//include Mongodb
+const url = 'mongodb://localhost:27017/twitter';
+const db = require('monk')(url);
+const collection = db.get('tweets');
+//include Twitter API
+const Twitter = require('twitter');
+//create Twitter client object
+var secret = require('./secret.json');
+const client = new Twitter(secret);
+
+app.get('/stream', function (req, res) {
+  var stream = client.stream('statuses/filter', {track: 'ASM'});
+  stream.on('data', function(event) {
+    collection.insert(event);
+    console.log('tweet added!');
+    //console.log(event.text);
+  });
+});
+
+app.get('/tweets', function (req, res) {
+  collection.find({}).then((docs) => {
+    res.json(docs);
+  });
+});
+
+app.listen(3000, function () {
+  console.log('listening on port 3000!');
+});
